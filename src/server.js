b1fe9f37abb0359222147434e2e835c2cd9cc7ad
João Paulo <joao.paulomonteiro@hotmail.com>
2023-02-17 11:19:17 -0300

    post method and json

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
-app.get("/", (request, response) => {
-  response.send("Hello, world!")
+app.post("/users", (request, response) => {
+  const { name, email, password } = request.body;
+
+  response.json({ name, email, password });
 });
 
 const PORT = 3333;
