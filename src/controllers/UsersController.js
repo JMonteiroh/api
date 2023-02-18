f6a486f26b27452a0a90457a85d0fd1dfd9d7d4b
João Paulo <joao.paulomonteiro@hotmail.com>
2023-02-18 14:36:53 -0300

    error handling

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,3 +1,5 @@
+const AppError = require("../utils/AppError");
+
 class UsersController {
  /**
   * index - GET para listar vários registros.
@@ -9,9 +11,13 @@ class UsersController {
 
   create(request, response) {
     const { name, email, password } = request.body;
+    if (!name) {
+      throw new AppError("Nome é obrigatório.");
+    }
 
     response.status(201).json({ name, email, password });
   }
 }
 
 module.exports = UsersController
+
