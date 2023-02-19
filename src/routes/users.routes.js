c886d87327dfe1985354bb78118e4662eddb5f5c
João Paulo <joao.paulomonteiro@hotmail.com>
2023-02-18 22:35:25 -0300

    update name, email, password

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,17 +1,12 @@
-const { Router, request, response } = require("express");
+const { Router } = require("express");
 
 const UsersController = require("../controllers/UsersController");
 
 const usersRoutes = Router();
 
-function myMiddleware(request, response, next) {
-  console.log("Voce passou pelo middleware")
-
-  next();
-}
-
 const usersController = new UsersController();
 
-usersRoutes.post("/", myMiddleware, usersController.create);
+usersRoutes.post("/", usersController.create);
+usersRoutes.put("/:id", usersController.update);
 
 module.exports = usersRoutes;
